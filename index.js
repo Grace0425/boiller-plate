08444a477e23109c073ded8f01b6a4e02fecf2d7
Grace0425 <79395584+Grace0425@users.noreply.github.com>
2021-11-21 16:30:12 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key')
+
+//데이터를 parse 해서 import
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jihye:abcd1234@boilerplate.mhde1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {}).then(() => console.log(`MongoDB Connedted...`))
+mongoose.connect(config.mongoURI, {}).then(() => console.log(`MongoDB Connedted...`))
     .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('hi!'))
+
+//회원가입을 위한 route
+app.post('/register', (req, res) => {
+    //회원 가입 시에 필요한 정보를 client에서 가져와 db에 넣음
+    const user = new User(req.body)//body-parser가 있어서 사용가능
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
